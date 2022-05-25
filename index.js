6a281cabbfa8be8f3de316fe5e8f48e3023553b4
favourite1975 <godsfavour1975@gmail.com>
2022-05-25 02:26:32 +0000

    updates were made

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-
+let body, colors, choice, demo;
 change.onclick = () =>{
-    let body = document.getElementsByTagName('Body')[0];
-    let colors = ['gainsboro','lightgray','silver','darkgray','gray','dimgray','black','cornsilk','#FA6800','bisque'
+    body = document.getElementsByTagName('Body')[0];
+    
+    colors = ['gainsboro','lightgray','silver','darkgray','gray','dimgray','black','cornsilk','#FA6800','bisque'
                 ,'wheat','steel','tan','rosybrown','goldenrod','peru','chocolate','sienna','brown','maroon','indianred'
                 ,'red','blue','green','white','pink','hotpink','orangered','violet','purple','darkviolet'];
-    let choice = Math.floor(Math.random()* colors.length);
+    choice = Math.floor(Math.random()* colors.length);
     body.style.backgroundColor = colors[choice];
     if (colors[choice] == 'black') {
         body.style.color = 'white';
     }
-    console.log(choice)
+    demo = document.getElementById('demo');
+    demo.innerHTML = (colors[choice]);
     
 }
